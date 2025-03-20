338822c4c24b4e1caa44229b7181d588caddea9a
Swarnava <swarn@SWARNAVASTEVE>
2025-03-21 02:05:07 +0530

    added matrices.lean

---
+++ KMTT/Graph.lean
@@ -0,0 +1,44 @@
+import Mathlib
+
+#print SimpleGraph
+#print Nonempty
+
+def g₁ : SimpleGraph Nat :=
+  ⟨ fun n m => n = m + 1 ∨ n = m - 1,sorry,sorry⟩
+
+#check SimpleGraph.isTree_iff
+
+namespace SimpleGraph
+
+
+variable {V : Type} [DecidableEq V]
+
+def acyclic (G : SimpleGraph V) : Prop :=
+  ∀ (v : V) (p : Walk G v v), ¬p.IsCircuit
+
+def Vertices (G : SimpleGraph V) : Set V :=
+  { v | ∃ w, G.Adj v w ∨ G.Adj w v }
+
+def connected (G : SimpleGraph V) : Prop :=
+  Nonempty <| ∀ (v w : V), Walk G v w
+
+def tree (G : SimpleGraph V) : Prop :=
+  connected G ∧ acyclic G
+
+def Edges (G : SimpleGraph V) : Set (V × V) :=
+  { e | G.Adj e.1 e.2 }
+
+def spanningTree (G : SimpleGraph V) (T : SimpleGraph V) : Prop :=
+  tree T ∧ T.Edges ⊆ G.Edges
+
+#print Set
+
+end SimpleGraph
+def Set.cardinality {α : Type}  (s : Set α) [Fintype s] : Nat :=
+  (s.toFinset).card
+
+def subsets {α : Type} (s : Set α) (n : Nat) : Set (Set α) :=
+  { t | t ⊆ s ∧ @Set.cardinality _ t = n }
+
+def spanningtrees {V : Type} [DecidableEq V] (G : SimpleGraph V) : Set (SimpleGraph V) :=
+  { T | SimpleGraph.tree T ∧ T.Vertices = G.Vertices ∧ T.Edges ⊆ G.Edges }
---
+++ KMTT/Matrices.lean
@@ -0,0 +1,51 @@
+import Mathlib
+--import Graph.SimpleGraph
+namespace SimpleGraph
+
+#print SimpleGraph.lapMatrix
+#print SimpleGraph.incMatrix    --need to understand
+#print SimpleGraph.adjMatrix
+
+variable {V : Type} [DecidableEq V]
+def Edges (G : SimpleGraph V) : Set (V × V) :=
+  { e | G.Adj e.1 e.2 }
+
+def IncidenceMatrix {V : Type} [DecidableEq V] (G : SimpleGraph V) : Matrix V G.Edges Int :=
+  fun v e => if v = e.val.1 then 1 else if v = e.val.2 then -1 else 0
+
+#print Matrix
+
+def IdentityMatrix (n : Nat) : Matrix (Fin n) (Fin n) Int :=
+  fun i j => if i = j then 1 else 0
+
+alias I := IdentityMatrix
+
+-- define permutations of length n in Fin m
+
+
+def listSubsetsOfSize {α : Type} : ℕ → List α → List (List α)
+  | 0, _ => [[]]
+  | _, [] => []
+  | (m+1), x :: xs =>
+      (listSubsetsOfSize m xs).map (fun ys => x :: ys) ++ listSubsetsOfSize (m+1) xs
+
+#eval listSubsetsOfSize 2 [1,2,3,4]
+
+#check Finset (Fin 2)
+
+noncomputable def submatrix {n m : ℕ} {α : Type} [Zero α] (A : Matrix (Fin n) (Fin m) α) (rows : Finset (Fin n)) (cols : Finset (Fin m)) : Matrix (Fin rows.card) (Fin cols.card) α :=
+  fun i j => A (rows.toList.get ⟨i.1, by simp [i.2]⟩) (cols.toList.get ⟨j.1, by simp [j.2]⟩)
+
+
+lemma toprove_cauchybinet {m n : Nat} (A : Matrix (Fin n) (Fin m) Int) (B : Matrix (Fin m) (Fin n) Int) :
+  ∀ a : Int, a^m * (a • (I n) + A * B).det = a^n * (a • (I m) + B * A).det :=
+  sorry
+
+theorem CauchyBinet {m n : Nat} (M : Matrix (Fin m) (Fin n) Int) (N : Matrix (Fin n) (Fin m) Int) :
+  (M * N).det = ∑ sorry
+
+#check CategoryTheory.Mat.id_apply_self
+
+
+
+end SimpleGraph
