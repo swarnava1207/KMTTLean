cf82b3c79ec52ee7f8372f92e101d473e92686a7
swarnava1207 <swarnavac@iisc.ac.in>
2025-03-23 02:25:06 +0530

    Add theorems.lean

--- KMTT/Matrices.lean
+++ KMTT/Matrices.lean
@@ -20,7 +20,7 @@ def IdentityMatrix (n : Nat) : Matrix (Fin n) (Fin n) Int :=
 
 alias I := IdentityMatrix
 
--- define permutations of length n in Fin m
+
 
 
 def listSubsetsOfSize {α : Type} : ℕ → List α → List (List α)
@@ -37,14 +37,47 @@ noncomputable def submatrix {n m : ℕ} {α : Type} [Zero α] (A : Matrix (Fin n
   fun i j => A (rows.toList.get ⟨i.1, by simp [i.2]⟩) (cols.toList.get ⟨j.1, by simp [j.2]⟩)
 
 
+#check Finset.powersetCard
+
+def all_subsets_of_size (n m : ℕ) : Finset (Finset (Fin m)) :=
+  Finset.powersetCard n (Finset.univ)
+
+#check all_subsets_of_size 2 3
+#eval all_subsets_of_size 2 3
+
+#synth Fintype (Fin 3)
+
 lemma toprove_cauchybinet {m n : Nat} (A : Matrix (Fin n) (Fin m) Int) (B : Matrix (Fin m) (Fin n) Int) :
   ∀ a : Int, a^m * (a • (I n) + A * B).det = a^n * (a • (I m) + B * A).det :=
   sorry
 
+/--
+error: typeclass instance problem is stuck, it is often due to metavariables
+  Fintype (?m.49370 s)
+-/
+#guard_msgs in
 theorem CauchyBinet {m n : Nat} (M : Matrix (Fin m) (Fin n) Int) (N : Matrix (Fin n) (Fin m) Int) :
-  (M * N).det = ∑ sorry
+  (M * N).det = ∑ s ∈ all_subsets_of_size n m, (submatrix M s _).det * (submatrix N _ s).det :=
+  by sorry
 
-#check CategoryTheory.Mat.id_apply_self
+#check Finset.card_erase_le
+
+#leansearch "0 as an element of Fin n?"
+
+#check Nat.pos_of_ne_zero
+
+lemma removal_decrease_card {α : Type} [DecidableEq α] [Fintype α] (s : Finset α) (a : α) : (Finset.erase s a).card < s.card :=
+  by
+
+
+#check Finset.erase
+
+def Matrix.minor {m n : ℕ} {α : Type} [Zero α] (A : Matrix (Fin m) (Fin n) α) (h₁ : m ≠ 0) (h₂ : n ≠ 0) : Matrix (Fin (m-1)) (Fin (n-1)) α :=
+  submatrix A (Finset.univ.erase ⟨0, by apply Nat.pos_of_ne_zero h₁⟩) (Finset.univ.erase ⟨0, by apply Nat.pos_of_ne_zero h₂⟩)
+
+
+lemma inc_lap {V : Type} [DecidableEq V] (G : SimpleGraph V) :
+
 
 
 
